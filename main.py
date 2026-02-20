d70027c9776fa962505992a47f526748afc20837
Musstavo <mohdtantour@gmail.com>
2026-02-20 19:02:26 +0300

    typical dice score

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+
+y_true = torch.zeros((10, 10))
+y_true[:, 3] = 1
+
+y_pred = y_true.clone()
+y_pred[:, 3][5] = 0
+# print(f"{y_pred=}")
+# print(f"{y_true=}")
+
+overlap = (y_pred * y_true).sum()
+total_true = y_true.sum()
+total_pred = y_pred.sum()
+
+dice_score = (2 * overlap) / (total_true + total_pred)
+# print(f"{dice_score=}")
+
+
